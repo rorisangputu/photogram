a1376d6484c18f9e51a8f7f68f4118fbf730993f
Rorisang Putu <rorisangputu@gmail.com>
2024-10-09 01:16:26 +0200

    creating function to protect certain routes

---
+++ src/components/ProtectedRoutes/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+
+interface IProtectedRoutesProps {
+}
+
+const ProtectedRoutes: React.FunctionComponent<IProtectedRoutesProps> = (props) => {
+    const isAuth: boolean = true;
+    const location = useLocation(); //gives path that is in browser url. 
+
+    //If user is authenticated all protected routes will be displayed
+    return isAuth ? (<Outlet/>): (
+        <Navigate to="/login" state={{from: location }} /> //redirects to login from which page
+    );
+};
+
+export default ProtectedRoutes;
