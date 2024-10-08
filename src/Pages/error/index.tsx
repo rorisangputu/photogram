27628366468a7be072001231d2f76145dd78725d
Rorisang Putu <rorisangputu@gmail.com>
2024-10-09 00:52:14 +0200

    creating error page

---
+++ src/Pages/error/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface IErrorProps {
+}
+
+const Error: React.FunctionComponent<IErrorProps> = (props) => {
+    return (
+        <div>
+            <h1>Error</h1>
+        </div>
+    );
+};
+
+export default Error;
