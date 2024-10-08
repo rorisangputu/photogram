09fd96e938c2e53284078f0b9ad03a22fc90d7bf
Rorisang Putu <rorisangputu@gmail.com>
2024-10-09 01:20:10 +0200

    adding children to protected routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,30 +5,37 @@ import Home from "./Pages/home";
 import SignUp from "./Pages/signup";
 import Profile from "./Pages/profile";
 import MyPhotos from "./Pages/myphotos";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 const router = createBrowserRouter([
     {
-        path: '/',
-        element: <Home />,
-        errorElement: <Error/>
+        element: <ProtectedRoutes />,
+        children:[
+            {
+                path: '/',
+                element: <Home />,
+                errorElement: <Error/>
+            },
+            {
+                path: '/profile',
+                element: <Profile/>,
+                errorElement: <Error/>
+            },
+            {
+                path: '/myphotos',
+                element: <MyPhotos />,
+                errorElement: <Error/>
+            },
+        ],
     },
-    {
-        path: '/profile',
-        element: <Profile/>,
-        errorElement: <Error/>
-    },
-    {
-        path: '/myphotos',
-        element: <MyPhotos />,
-        errorElement: <Error/>
-    },
+    
     {
         path: '/login',
         element: <Login />,
         errorElement: <Error/>
     },
     {
-        path: '/login',
+        path: '/signup',
         element: <SignUp />,
         errorElement: <Error/>
     },
