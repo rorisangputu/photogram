0f72cea32104e7ad1b2152b0a401273d29620bd2
Rorisang Putu <rorisangputu@gmail.com>
2024-10-09 01:04:01 +0200

    creating routes

---
+++ src/routes.tsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login from './Pages/login'
+import Error from './Pages/error'
+import Home from "./Pages/home";
+import SignUp from "./Pages/signup";
+import Profile from "./Pages/profile";
+import MyPhotos from "./Pages/myphotos";
+export const router = createBrowserRouter([
+    {
+        path: '/home',
+        element: <Home />,
+        errorElement: <Error/>
+    },
+    {
+        path: '/profile',
+        element: <Profile/>,
+        errorElement: <Error/>
+    },
+    {
+        path: '/myphotos',
+        element: <MyPhotos />,
+        errorElement: <Error/>
+    },
+    {
+        path: '/login',
+        element: <Login />,
+        errorElement: <Error/>
+    },
+    {
+        path: '/login',
+        element: <SignUp />,
+        errorElement: <Error/>
+    },
+
+])
