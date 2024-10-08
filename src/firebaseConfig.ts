d36e8f19e6647ba1e852a34888796a037aaadb6b
Rorisang Putu <rorisangputu@gmail.com>
2024-10-08 23:34:35 +0200

    adding firebase config

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAFNQoFjFr8ptaDlBAleBHmdVlgPIA0Zc",
+  authDomain: "photogram-b5cc1.firebaseapp.com",
+  projectId: "photogram-b5cc1",
+  storageBucket: "photogram-b5cc1.appspot.com",
+  messagingSenderId: "28322832251",
+  appId: "1:28322832251:web:968b5e6ce4a4d9ea07e7de"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
