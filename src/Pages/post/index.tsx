7bfdbf82fc66192d288e0f96fa403803f67f4e7f
Rorisang Putu <rorisangputu@gmail.com>
2024-10-09 00:52:39 +0200

    creating post page

---
+++ src/Pages/post/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+interface IPostProps {
+}
+
+const Post: React.FunctionComponent<IPostProps> = (props) => {
+  return <div>Post</div>;
+};
+
+export default Post;
