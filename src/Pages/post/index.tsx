16f1bf49378040acc9b95f07a56640a1962bed5c
Rorisang Putu <rorisangputu@gmail.com>
2024-10-12 23:40:32 +0200

    adding components to create post page

--- src/Pages/post/index.tsx
+++ src/Pages/post/index.tsx
@@ -1,4 +1,5 @@
 import Layout from '@/components/layout';
+import { Label } from '@/components/ui/label';
 import * as React from 'react';
 
 interface ICreatePostProps {
@@ -7,7 +8,18 @@ interface ICreatePostProps {
 const CreatePost: React.FunctionComponent<ICreatePostProps> = (props) => {
   return (
     <Layout>
-      <div>Create Post</div>
+      <div className='flex justify-center'>
+        <div className='border max-w-3xl w-full'>
+          <h3 className='bg-slate-800 text-white text-center text-lg p-2'>Create Post</h3>
+          <div className="p-8">
+            <form action="">
+              <div className="flex flex-col">
+                <Label className='mb-4' htmlFor='caption'>Photo caption</Label>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
