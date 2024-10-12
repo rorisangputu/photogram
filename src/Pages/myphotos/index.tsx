77e6e439143549dfe94b91e73251a82b2f585707
Rorisang Putu <rorisangputu@gmail.com>
2024-10-12 22:44:55 +0200

    adding layout to my photos page

--- src/Pages/myphotos/index.tsx
+++ src/Pages/myphotos/index.tsx
@@ -1,10 +1,15 @@
+import Layout from '@/components/layout';
 import * as React from 'react';
 
 interface IMyPhotosProps {
 }
 
 const MyPhotos: React.FunctionComponent<IMyPhotosProps> = (props) => {
-  return <div>My Photos</div> ;
+  return (
+    <Layout>
+      <div>My Photos</div>
+    </Layout>
+  );
 };
 
 export default MyPhotos;
